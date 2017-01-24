25cfcdf1bcd7815cda63bfe5816bdbdef33d0f96
Peter Witham <peter@peterwitham.com>
2017-01-24 16:09:06 -0600

    Updated orientation display of master list when returning from background, simplified initial orientation view detection.

--- Code Notes/AppDelegate.swift
+++ Code Notes/AppDelegate.swift
@@ -20,12 +20,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
         splitViewController.delegate = self
         configureUserSettings()
-        if UIScreen.main.bounds.height > UIScreen.main.bounds.width {
-            splitViewController.preferredDisplayMode = .primaryOverlay
-        }
+        splitViewController.preferredDisplayMode = .automatic
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
