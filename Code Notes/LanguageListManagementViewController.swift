4181e16a0d4177fc30b12f5839e67bc2b2e7a154
Peter Witham <peter.witham@gmail.com>
2017-02-06 20:45:13 -0600

    WIP Added stub functions for managing the language list.

---
+++ Code Notes/LanguageListManagementViewController.swift
@@ -0,0 +1,46 @@
+//
+//  LanguageListManagementViewController.swift
+//  Code Notes
+//
+//  Created by Peter Witham on 2/6/17.
+//  Copyright © 2017 Peter Witham. All rights reserved.
+//
+
+import UIKit
+
+class LanguageListManagementViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func btnAddLanguage(_ sender: UIButton) {
+    }
+
+    @IBAction func btnRemoveLanguage(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnResetLanguages(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnClearAllLanguages(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
